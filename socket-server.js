4384a2cf4e4d5ebbbe236b11987a39259df8d0f7
Sasha <teer12@yandex.ua>
2017-01-12 18:31:01 +0200

    less-7 end

--- socket-server.js
+++ socket-server.js
@@ -10,9 +10,23 @@ app.get('/', function(req, res){
 });
 
 io.sockets.on('connection',function(client){
-	console.log('Connected');
-	client.emit('message', {hello : 'Guest'});
+	//console.log('Connected');
+	//client.emit('message', {hello : 'Guest'});
 	client.on('message',function(data){
-		console.log(data);
+		//client.set('nickname',data);
+		client.emit('hello', {hello : 'Hello, '+data});
+		client.broadcast.emit('hello', {hello : 'Hello, from '+data});
+		//io.sockets.emit('hello', {hello : 'Hello, everybody'});
+	});
+	client.on('disconnect',function(){
+		io.sockets.emit('hello', {hello : 'One of us is enter!'});
+	});
+	client.on('new_message',function(data){
+		client.emit('hello', {hello : 'Hello, '+data});
+		//client.get('nickname',function(err, oldName){
+			//client.broadcast.emit('hello', {hello : oldName + ' now is '+data});
+		//});
+		//client.set('nickname',data);
+		//client.broadcast.emit('hello', {hello : 'Hello, from '+data});
 	});
 });
