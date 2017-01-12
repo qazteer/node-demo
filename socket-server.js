ca43a75297765bac29c187d4e7f7e7bde3001ad4
Sasha <teer12@yandex.ua>
2017-01-12 16:16:55 +0200

    add socket-server.js

--- socket-server.js
+++ socket-server.js
@@ -1,3 +1,4 @@
+//node socket-server.js
 var socket = require('socket.io');
 var express = require('express');
 
@@ -5,9 +6,13 @@ var app = express();
 var io = socket.listen(app.listen(8080));
 
 app.get('/', function(req, res){
-	res.send('Hello socket!');
+	res.sendfile(__dirname + '/index2.html');
 });
 
 io.sockets.on('connection',function(client){
 	console.log('Connected');
+	client.emit('message', {hello : 'Guest'});
+	client.on('message',function(data){
+		console.log(data);
+	});
 });
